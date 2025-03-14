4dd38d60acf0d163e179b20c1c10e886a5d4ecf4
Simon Bashir <simon_bashir@yahoo.com>
2025-03-14 10:28:46 -0500

    config path resolver for utils

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import react from "@vitejs/plugin-react"
-import type { UserConfig } from "vite"
-import path from "path"
 
 export default {
   plugins: [react()],
   base: "./",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 } satisfies UserConfig
